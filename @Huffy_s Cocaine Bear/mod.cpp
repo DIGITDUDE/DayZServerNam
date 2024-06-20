e06a13e231744ecd7e6da0168e24106e93a43a03
ServerMitch <Rvaneck15@gmail.com>
2024-06-20 12:34:05 +0100

    .

---
+++ @Huffy_s Cocaine Bear/mod.cpp
@@ -0,0 +1,10 @@
+name = "Huffy's Cocaine Bear";
+picture = "Huffy_Cocaine_Bear/imagesets/Huffys_CocaineBear_smalllogo.edds";
+logoSmall = "Huffy_Cocaine_Bear/imagesets/Huffys_CocaineBear_smalllogo.edds";
+logo = "Huffy_Cocaine_Bear/imagesets/Huffys_CocaineBear_smalllogo.edds";
+logoOver = "Huffy_Cocaine_Bear/imagesets/Huffys_CocaineBear_smalllogo_hover.edds";
+action = "https://steamcommunity.com/profiles/76561198072019228/myworkshopfiles/?appid=221100";
+tooltip = "Huffy's Cocaine Bear";
+overview = "Huffy's Cocaine Bear";
+author = "Huffy";
+hidePicture = 0;
