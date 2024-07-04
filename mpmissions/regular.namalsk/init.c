fd7cf509bd0d6a4e3ce92b1886d2406ba43a1b7a
Rolf van Eck <108688509+DIGITDUDE@users.noreply.github.com>
2024-07-04 12:02:45 +0200

    init with custom script test

--- mpmissions/regular.namalsk/init.c
+++ mpmissions/regular.namalsk/init.c
@@ -1,3 +1,4 @@
+#include <CustomScripts/override void Expansion_OnQuestStart(Exp.c>
 void main()
 {
 	/*
@@ -118,30 +119,8 @@ class CustomMission: MissionServer
 		player.GetStatEnergy().Set( 1100 );
 	}
 
-	override void Expansion_OnQuestStart(ExpansionQuest quest){
-			ref array<EntityAI> m_SpawnedZombies = new array<EntityAI>;
-			ExpansionQuestConfig questConfig = quest.GetQuestConfig();
-			switch (questConfig.GetID()){
-				case 1011:
-				{
-					vector pos = SnapToGround(Vector(5513.33,117.502,8141.14));
-                	string zmbClass = "Animal_HMG_Bear_Cocaine_T0";
-                	EntityAI AIzmb = GetGame().CreateObject( zmbClass, pos, false, true );
-                	ref AnimalBase Bszmb;
-                	Class.CastTo(Bszmb,AIzmb);
-                	// Bszmb.AttachEventHandle(PossibleLootDrops,PossibleWeaponDrops,dropChance); for later use
-					  m_SpawnedZombies.Insert(AIzmb);
-                	  //  SpawnObject(zmbClass , pos);
-				}
-			break;
-		}
+	InitCustom();
 
-	}
-	void SpawnObject( string type, vector position,)
-	{
-    auto obj = GetGame().CreateObject( type, position );
-    obj.SetPosition( position );
-    }
 };
   
 Mission CreateCustomMission(string path)
