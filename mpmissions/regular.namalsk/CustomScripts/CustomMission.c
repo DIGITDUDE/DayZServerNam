8ea970a3a566fea413bc6d3264ecee0796d19858
Rolf van Eck <108688509+DIGITDUDE@users.noreply.github.com>
2024-07-04 12:56:19 +0200

    .

--- mpmissions/regular.namalsk/CustomScripts/CustomMission.c
+++ mpmissions/regular.namalsk/CustomScripts/CustomMission.c
@@ -7,23 +7,23 @@ override void Expansion_OnQuestStart(ExpansionQuest quest){
                 case 1011:
                 {
                         vector pos = SnapToGround(Vector(5513.33,117.502,8141.14));
-        string zmbClass = "Animal_HMG_Bear_Cocaine_T0";
-        EntityAI AIzmb = GetGame().CreateObject( zmbClass, pos, false, true );
-        ref AnimalBase Bszmb;
-        Class.CastTo(Bszmb,AIzmb);
-        // Bszmb.AttachEventHandle(PossibleLootDrops,PossibleWeaponDrops,dropChance); for later use
-                          m_SpawnedZombies.Insert(AIzmb);
-          //  SpawnObject(zmbClass , pos);
+                        string zmbClass = "Animal_HMG_Bear_Cocaine_T0";
+                        EntityAI AIzmb = GetGame().CreateObject( zmbClass, pos, false, true );
+                        ref AnimalBase Bszmb;
+                        Class.CastTo(Bszmb,AIzmb);
+                        // Bszmb.AttachEventHandle(PossibleLootDrops,PossibleWeaponDrops,dropChance); for later use
+                         m_SpawnedZombies.Insert(AIzmb);
+                        //  SpawnObject(zmbClass , pos);
                 }
         break;
     }
 
-};
-
-};
+}
 
 void SpawnObject( string type, vector position)
 {
 auto obj = GetGame().CreateObject( type, position );
 obj.SetPosition( position );
+}
+
 };
